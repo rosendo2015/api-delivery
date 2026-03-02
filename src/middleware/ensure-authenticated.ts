be764657a5485490d2f7b32db513df84fb9e58f2
rosendo <rosendc30@gmail.com>
2026-03-01 22:51:19 -0300

    authenticated

---
+++ src/middleware/ensure-authenticated.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express"
+import { verify } from "jsonwebtoken"
+import { AppError } from "@/utils/AppError"
+import { authConfig } from "@/configs/auth"
+
+interface TokenPayload {
+    sub: string
+    role: string
+}
+
+function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+    try {
+        const authHeader = request.headers.authorization;
+        if (!authHeader) {
+            throw new AppError("JWT token not found", 401);
+        }
+        const [, token] = authHeader.split(" ");
+        const { role, sub: user_id } = verify(token, authConfig.jwt.secret) as TokenPayload;
+
+        request.user = {
+            id: user_id,
+            role
+        };
+        return next();
+    } catch (error) {
+        throw new AppError("Invalid JWT token", 401);
+    }
+}
