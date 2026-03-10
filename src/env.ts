cb52ca80a9be49e6d8c27c3d086dff3db5d17a89
rosendo <rosendc30@gmail.com>
2026-03-10 20:30:34 -0300

    testes

--- src/env.ts
+++ src/env.ts
@@ -11,6 +11,7 @@ dotenv.config({
 const envSchema = z.object({
     DATABASE_URL: z.url(),
     JWT_SECRET: z.string().min(1),
+    PORT: z.coerce.number().default(3333),
 });
 
 // Faz o parse e valida
--- src/tests/sessions-controller.test.ts
+++ src/tests/sessions-controller.test.ts
@@ -1,13 +1,28 @@
 import request from "supertest";
 import { app } from "@/app";
+import { prisma } from "@/database/prisma";
 
 describe("SessionsController", () => {
+    let user_id: string;
+    afterAll(async () => {
+        // Clean up the created user after tests
+        await prisma.user.delete({ where: { id: user_id } });
+    })
+
     it("should create a new session successfully", async () => {
-        const response = await request(app).post("/sessions").send({
-            email: "john.doe@example.com",
+        const userResponse = await request(app).post("/users").send({
+            name: "John Doe",
+            email: "alth.john.doe@example.com",
+            password: "123456",
+        })
+        user_id = userResponse.body.id; // Store the created user's ID for cleanup  
+
+        const sessionResponse = await request(app).post("/sessions").send({
+            email: "alth.john.doe@example.com",
             password: "123456",
         });
-        expect(response.status).toBe(201);
-        expect(response.body).toHaveProperty("token");
+        expect(sessionResponse.status).toBe(200);
+        expect(sessionResponse.body.token).toEqual(expect.any(String));
+
     });
 });
